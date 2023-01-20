a2459cbe53dada75da7f271489eb78393bad1b05
Oleks <{ID}+{username}@users.noreply.github.com>
2023-01-19 20:23:16 -0500

    make the axios call to get posts

--- src/Component/Post.tsx
+++ src/Component/Post.tsx
@@ -6,7 +6,7 @@ interface PostProps {
 }
 
 const Post: React.FC<PostProps> = () => {
-  const [postData, setPostData] = useState(null);
+  const [postData, setPostData] = useState([]);
   const [error, setError] = useState(null);
   const apiPath = "http://localhost:8080/api/posts/getAllPosts";
   useEffect(() => {
@@ -14,16 +14,22 @@ const Post: React.FC<PostProps> = () => {
       .get(apiPath)
       .then((response) => {
         setPostData(response.data);
-        console.log(response.data);
+        console.log(response.data[0]);
       })
       .catch((error) => {
         setError(error);
       });
   }, []);
+let loadData = () => {
+  for(let key in postData[0]){
+    console.log("Key "+ key, "value " + postData[0][key])
+    return "Key "+ key + "value " + postData[0][key];
+  }
+}
 
   return (
     <>
-      <div>{postData}</div>
+      <div>{loadData()}</div>
     </>
   );
 };
